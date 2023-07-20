d1ee77726f9a6cec57c0991b6ba7e25f18f6bc6e
Elise Ngo <elisetvy@gmail.com>
2023-07-20 11:19:44 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', function () {
+  let nav = document.querySelector('.header_nav');
+  let windowHeight = window.innerHeight;
+  let scrollPosition = document.documentElement.scrollTop;
+
+  if (scrollPosition >= windowHeight / 8) {
+    nav.style.visibility = 'hidden';
+  } else {
+    nav.style.visibility = 'visible';
+  }
+});
+
+window.addEventListener('scroll', function () {
+  let backToTop = document.getElementById('back-to-top-btn');
+  let logo = document.getElementById('header_logo');
+  let about = document.getElementById('about');
+  let resume = document.getElementById('resume');
+  let contact = document.getElementById('contact');
+
+  let aboutTop = about.offsetTop;
+  let resumeTop = resume.offsetTop;
+  let contactTop = contact.offsetTop;
+  let scrollPosition = document.documentElement.scrollTop;
+
+  if (scrollPosition >= aboutTop && scrollPosition < resumeTop || scrollPosition >= contactTop) {
+    backToTop.style.color = '#EE3250';
+    logo.style.backgroundColor = '#EE3250';
+    logo.style.color = 'white';
+  } else {
+    backToTop.style.color = 'white';
+    logo.style.backgroundColor = 'white';
+    logo.style.color = '#EE3250';
+
+  }
+});
+
+let photo = document.querySelector('.photo');
+
+photo.addEventListener('mouseover', function () {
+  photo.style.zIndex = '9997';
+});
+
+photo.addEventListener('mouseout', function () {
+  photo.style.zIndex = '-9999';
+});
